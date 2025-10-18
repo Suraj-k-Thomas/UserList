806fdc617461e7d65c75de1076e43e5c2a7e9a55
surajkthomas <ksurajt@gmail.com>
2025-10-18 12:55:13 +0530

    throws Error on client error

--- UserListTests/UserListTests.swift
+++ UserListTests/UserListTests.swift
@@ -20,7 +20,32 @@ class UserListTests :XCTestCase{
     }
 
     
-    func test_RequestDataFromLoad () {
+    func test_RequestDataFromLoad ()  async {
+        
+        let url =  URL(string: "https://a-url.com")!
+        let (sut , client) = makesut(url: url)
+       _ = try? await  sut.load()
+        let requestedUrls = await client.requestedUrls
+        XCTAssertEqual(requestedUrls, [url])
+    }
+    
+    func  test_ThrowsErrorOnClinetError () async {
+        
+        let (sut , client) = makesut()
+        await client.enqueue(response: .failure(NSError(domain: "error", code: 0, userInfo: nil)))
+        
+        
+        do {
+          _ =  try await sut.load()
+            
+        }
+        catch  let error as UsersList.Error {
+            
+            XCTAssertEqual(error ,UsersList.Error.connectivity)
+        }
+        catch{
+            XCTFail("error")
+        }
         
         
     }
@@ -46,22 +71,62 @@ protocol HTTPClient {
 
 actor ClientSpy : HTTPClient {
     
+    enum Response {
+        case success (Data , HTTPURLResponse)
+        case failure (Error)
+        
+    }
+    
+    var queue : [Response] = []
+    
+    func enqueue(response : Response) {
+        
+        queue.append(response)
+    }
+    
     var requestedUrls : [URL] = []
     func get(from url: URL) async throws -> (Data, HTTPURLResponse) {
-        throw URLError(.badURL)
-
+        requestedUrls.append(url)
+        
+        guard !queue.isEmpty else {throw URLError(.badURL)}
+        let item = queue.removeFirst()
+        switch item {
+        case .success(let data, let uRLResponse):
+            return (data , uRLResponse)
+        case .failure(let error):
+            throw error
+        }
     }
     
 }
 
 
 class UsersList {
+    
+  public enum Error : Swift.Error {
+        
+        case connectivity
+        case invalidResponse
+    }
+    
+    
     let url : URL
     let client : HTTPClient
     
     init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
+    }
+    
+    func load () async throws -> [UsersList] {
+        
+        do {
+          _ =  try await client.get(from: url)}
+        catch  {
+            throw Error.connectivity
+        
+        }
+        return []
     }
         
 }
