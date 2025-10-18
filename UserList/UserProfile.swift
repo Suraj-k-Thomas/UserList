c2fcd82740f2ca307eced7431d21522f1242b6c2
surajkthomas <ksurajt@gmail.com>
2025-10-18 11:32:34 +0530

    UserProfile model added

---
+++ UserList/UserProfile.swift
@@ -0,0 +1,22 @@
+//
+//  UserProfile.swift
+//  UserList
+//
+//  Created by Suraj  Thomas on 18/10/25.
+//
+
+import Foundation
+
+public struct USerProfile {
+    let id: UUID
+        let name: String
+        let company: String
+        let username: String
+        let email: String
+        let address: String
+        let zip: String
+        let state: String
+        let country: String
+        let phone: String
+        let photo: String
+}
--- UserListTests/UserListTests.swift
+++ UserListTests/UserListTests.swift
@@ -7,11 +7,11 @@
 
 import Testing
 @testable import UserList
+import Foundation
 
-struct UserListTests {
+class UserListTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
 
 }
+
+
