505a867de4c925d03a89123ea44ced7451fad6cc
surajkthomas <ksurajt@gmail.com>
2025-10-18 10:40:09 +0530

    Initial Commit

---
+++ UserList/UserListApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserListApp.swift
+//  UserList
+//
+//  Created by Suraj  Thomas on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct UserListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UserListTests/UserListTests.swift
@@ -0,0 +1,17 @@
+//
+//  UserListTests.swift
+//  UserListTests
+//
+//  Created by Suraj  Thomas on 18/10/25.
+//
+
+import Testing
+@testable import UserList
+
+struct UserListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
